72bf10d04f6b6201fce07fb6423f22a0fd7fc470
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2024-08-10 07:36:35 +0530

    Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Header: React.FC = () => {
+    return (
+        <div className="header-container">
+            <div className="header-text">Awesome Note Taker</div>
+        </div>
+    );
+};
+
+export default Header;
